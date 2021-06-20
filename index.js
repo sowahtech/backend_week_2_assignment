dda7718ab7c227b123aca6d8b27cf7ed02caee00
justice sowah <justice.sowah18@gmail.com>
2021-06-20 22:01:50 +0000

    request handlers created for delete and put requests

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ const handlePostSignUpRequest = (req, res) => {
     res.send("<h1>voila, welcome to the sign up page. please enter your details</h1>")
 }
 
-const  handlePutProfileRequest = (req, res) => {
-    res.send("<h1>Success. Your profile page updated</h1>")
+const  handleGetLoginRequest = (req, res) => {
+    res.send("<h1>Hello! Welcome to the login page</h1>")
 }
 
+const  handlePutProfileRequest = (req, res) => {
+    res.send("<h1>Success. Your profile page is updated</h1>")
+}
+
+const  handleDeleteOrderRequest = (req, res) => {
+    res.send("<h1>Success. Order deleted</h1>")
+}
 
 
 //creating a server instance
@@ -19,6 +26,7 @@ const server = express();
 server.post('/signUp', handlePostSignUpRequest);
 server.get('/login', handleGetLoginRequest);
 server.put('/profile', handlePutProfileRequest);
+server.delete('/order', handleDeleteOrderRequest);
 
 //starting the server
 server.listen(3000, () => console.log("server is ready to receive requests"))
