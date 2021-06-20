59fdee533b5a35d8f3b352d1e437a8af07f915c5
justice sowah <justice.sowah18@gmail.com>
2021-06-20 21:47:50 +0000

    request handler created for the get request to the login page

--- index.js
+++ index.js
@@ -2,16 +2,20 @@
 const express = require('express');
 
 //request handlers
-const handleGetSignUpRequest = (req, res) => {
+const handlePostSignUpRequest = (req, res) => {
     res.send("<h1>voila, welcome to the sign up page. please enter your details</h1>")
 }
 
+const handleGetLoginRequest = (req, res) => {
+    res.send("<h1>Login successful. Enjoy your stay</h1>")
+}
 
 //creating a server instance
 const server = express();
 
 //midddlewares
-server.post('/signUp', handleGetSignUpRequest);
+server.post('/signUp', handlePostSignUpRequest);
+server.get('/login', handleGetLoginRequest);
 
 //starting the server
 server.listen(3000, () => console.log("server is ready to receive requests"))
